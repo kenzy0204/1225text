6d7b65ef3f39c3f424ce1d362c0a454254406e4d
kenzy0204 <146816667+kenzy0204@users.noreply.github.com>
2023-12-25 15:19:57 +0800

    Add files via upload

---
+++ 1225-1.js
@@ -0,0 +1,11 @@
+function adjustText() {
+    const studentId = document.getElementById('studentId');
+    const name = document.getElementById('name');
+  
+    // Adjust font size and color
+    studentId.style.fontSize = '20px';
+    border.style.color = 'blue';  
+    name.style.fontSize = '20px';
+    name.style.color = 'green';
+  }
+  
